36819be010bff69d5c56421c65aac058177ac713
Ivkaran Singh <ivkaransingh@ivkarans-mbp.mynetworksettings.com>
2023-07-06 12:16:15 -0400

    sample test

---
+++ src/test/java/com/sampletest/TestNGTodo1.java
@@ -0,0 +1,63 @@
+package com.sampletest;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGTodo1 {
+
+    private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeMethod
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        System.setProperty("webdriver.chrome.driver", "/Users/ivkaransingh/Downloads/chromedriver_mac64/chromedriver");
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void basicTest() throws InterruptedException {
+
+        // Navigate to the Google homepage
+        driver.get("https://www.google.com");
+
+        // Locate the search text box
+        WebElement searchTextBox = driver.findElement(By.name("q"));
+
+        // Enter a search query
+        searchTextBox.sendKeys("Selenium");
+
+        Thread.sleep(2000);
+
+        // Click the search button
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+        searchButton.click();
+
+        // Verify that the search results page is displayed
+        String expectedTitle = "Selenium - Google Search";
+        String actualTitle = driver.getTitle();
+        assert expectedTitle.equals(actualTitle);
+
+        // Close the browser
+        driver.quit();
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
